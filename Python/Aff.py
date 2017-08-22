2b2ce6f8cd03f37b47329b8024d8800b4379ebe0
haha <wangzhe@xiongmaitech.com>
2017-08-22 20:29:27 +0800

    add test BeautifulSoup

--- Python/Aff.py
+++ Python/Aff.py
@@ -1,20 +1,27 @@
-#coding=utf-8
+﻿#coding=utf-8
 import io
 import sys
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-
-from bs4 import BeautifulSoup 
 import time
+import csv
+import os
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码 
-driver=webdriver.Chrome()# 得到浏览器 
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup 
+
+# 得到浏览器
+#driver = webdriver.PhantomJS()
+#本例中的网站仅支持Chrome
+driver=webdriver.Chrome()
+
 driver.get(r'https://app.afflow.rocks/')# 打开网页 
 # 定位到输入框并输入
 time.sleep(5)
 inputUsername = driver.find_element_by_xpath (r'//*[@id="username"]')
 inputPassword = driver.find_element_by_xpath (r'//*[@id="password"]')
-inputUsername.send_keys(r"你的帐号")
+inputUsername.send_keys(r"你的账号")
 inputPassword.send_keys(r"你的密码")
 # 提交 
 submitElement = driver.find_element_by_xpath (r'//*[@id="keyEntryContainer"]/div/div/div[3]/input')
@@ -31,23 +38,29 @@ ActionChains(driver).move_to_element(Menu).perform()
 liveLead = driver.find_element_by_xpath (r'//*[@id="menu_liveleadsButton"]')
 liveLead.click()
 
-time.sleep(3)
-web_data=driver.page_source		# 获取网页文件对象 
-#print (web_data)
+filename = 'Aff_lead.csv'
+csvfile = open(filename,'a')
+writer = csv.writer(csvfile)
 
+if not os.path.exists('Aff_lead.csv'):
+	print('Aff_lead.csv not exist')
+	item=['单价','转化时间','国家','类型','offerID','A/M']
+	writer.writerow(item)
 
-soup=BeautifulSoup(web_data,'lxml')# 解析网页 
-#获取这个页面需要的信息
-rows = soup.find_all('div', class_='newConversion')
-for row in rows:
-	print(row.span.strings)
+while True:
+	time.sleep(3)
+	web_data=driver.page_source		# 获取网页文件对象 
+	#print (web_data)
+	soup=BeautifulSoup(web_data,'lxml')# 解析网页 
+	#获取这个页面需要的信息
+	rows = soup.find_all('div', class_='newConversion')
+	for row in rows:
+		writer = csv.writer(csvfile,dialect='excel')
+		writer.writerow(list(row.stripped_strings))
+		print("写入成功：", end=' ')
+		print(list(row.stripped_strings))
 
-while False:
-	new_data_page = driver.page_source		# 获取网页文件对象
-	soup_newdata = BeautifulSoup(new_data_page,'lxml')# 解析网页
-	new_data = soup.find_all('div', class_='newConversion')
-
-
-soup=BeautifulSoup(web_data,'lxml')# 解析网页 
+driver.quit()
+csvfile.close()
 
 print ("爬虫完成")
---
+++ Python/BeautifulTest.py
@@ -0,0 +1,29 @@
+﻿#coding=utf-8
+import io
+import sys
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码 
+
+from bs4 import BeautifulSoup 
+
+
+filename = 'result.html'
+file = open(filename,'r', encoding='UTF-8')
+web_data = file.read()
+soup=BeautifulSoup(web_data,'lxml')# 解析网页 
+#获取这个页面需要的信息
+rows = soup.find_all('div', class_='newConversion')
+for row in rows:
+	print("写入成功：", end=' ')
+	#print(list(row.stripped_strings))
+	for x in row.contents:
+		try:
+			#print(x.attrs)
+			if len(x.attrs['class']) > 1:
+				if len(x.attrs['class'][1]) > 1:
+					print(x.attrs['class'][1])
+		except Exception as e:
+			pass
+	#for children in row.children:
+	#	print(children.tag)
+print ("完成")
